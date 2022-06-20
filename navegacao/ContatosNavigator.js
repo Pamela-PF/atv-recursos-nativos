db0ca811c823d06d4ac461632bfbbef7d571e774
Pamela <ni.pan1010@gmail.com>
2022-06-19 23:42:28 -0300

    Modificacoes no contatos Navigator

--- navegacao/ContatosNavigator.js
+++ navegacao/ContatosNavigator.js
@@ -4,13 +4,33 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import ListaDeContatos from '../telas/ListaDeContatos';
 import NovoContato from '../telas/NovoContato';
+import { HeaderButton, Item } from 'react-navigation-header-buttons';
+
+import BotaoCabecalho from '../componentes/BotaoCabecalho';
+
 
 const Stack = createNativeStackNavigator()
 
 const container = (
     <NavigationContainer>
         <Stack.Navigator initialRouteName='ListaDeContatos'>
-            <Stack.Screen name="ListaDeContatos" component={ListaDeContatos}/>
+            <Stack.Screen 
+            name="ListaDeContatos" 
+            component={ListaDeContatos}
+            options={(props) => ({
+                headerRight: () => <HeaderButton
+                HeaderButtonComponent={BotaoCabecalho}>
+                    <Item
+                        title="Adicionar"
+                        iconName='md-add'
+                        onPress={()=>{
+                            console.log("Chamou")
+                            props.navigation.navigate('NovoContato')
+                        }}
+                    />
+                </HeaderButton>
+            })}
+            />
             <Stack.Screen name="NovoContato" component={NovoContato}/>
         </Stack.Navigator>
     </NavigationContainer>
